7e6b61d55e50afb2437c081f6f5fc3ecb0a3a836
KararTY <alremahykarar@ymail.com>
2017-07-24 18:44:55 +0200

    Oops, fixed CRON example. Default `auto` value is `false`.

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  auto: false,
-  debug: false,
-  CRON: '0 * */1 * *',
-  authtoken: '',
-  guildID: ''
+  auto: false, // Will run automatically on a schedule based on 'CRON'.
+  debug: false, // Will display debug related messages to your console. (Really spammy.)
+  CRON: '0 0 */1 * *', // This example 'CRON' will run archiver every midnight (00:00).
+  authtoken: '', // Bot account is preferred.
+  guildID: '' // While in developer mode, right click your server icon and click 'Copy ID'.
 }
-// Cron help http://crontab.guru/
+// Cron help http://crontab.guru/ (Check this site out to see examples on scheduling)
