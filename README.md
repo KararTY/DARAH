de78b7807502fa333ee23adfea66f40d548650ac
Karar Al-Remahy <KararTY@users.noreply.github.com>
2018-02-04 05:17:20 +0100

    Version 1.2 - #3 Big servers support!
    Fixing #3 and adding new features.
      * Will now ask for guildID and authtoken if not provided.
      * acceptTOS variable available in settings.js for people who run CRON system scripts.

--- README.md
+++ README.md
@@ -1,31 +1,35 @@
-# ServerAutoRecordArchiverHeroine
-## Archives all Discord messages in a guild.
+# Server Auto Record Archiver Heroine
+## Archives **ALL** Discord messages in a guild.
 
 ### 10 Step Setup.
-  1. Get NodeJS, version 8.1.2 or newer.
-  2. Make sure you have enough disk space.
+  1. Get NodeJS, version **8.1.2 or newer**.
+  2. **Make sure you have enough disk space.**
   3. Git clone or download this repository and then change to the directory via console/terminal.
   4. Type `npm i` in your console/terminal and wait for dependencies to download and install successfully.
   5. Open up `settings.js` with any text program:
 ```js
 module.exports = {
-  auto: true, // Will run automatically on a schedule based on 'CRON'.
+  auto: false, // Will run automatically on a schedule based on 'CRON'.
   debug: true, // Will display debug related messages to your console. (Really spammy.)
   CRON: '0 0 */1 * *', // This example 'CRON' will run archiver every midnight (00:00).
+  acceptTOS: false, /* Make this 'true' if you're running your own CRON system script.
+  Please read this https://discordapp.com/developers/docs/legal before enabling. */
   authtoken: '', // Bot account is preferred.
-  guildID: '' // While in developer mode, right click your server icon and click 'Copy ID'.
+  guildID: '' // While in developer mode, right click the server icon and click 'Copy ID'.
+  messagesEveryFile: 100000 // Amount of max messages in every file. In effect limiting every file size at around 30-40 MB.
 }
 // Cron help http://crontab.guru/ (Check this site out to see examples on scheduling)
 ```
-  6. If `auto` is true the archiver will use the CRON job you've put in `CRON`.
+  6. If `auto` is true the archiver will parse the CRON string in settings.js variable `CRON`.
   7. `debug` is spammy. Don't enable it unless you want to know what's up via your console/terminal.
-  8. Change settings and most importantly: `authtoken` & `guildID` and then save.
-  9. Type `node app.js` in your console/terminal and wait for the program to successfully backup.
-  10. Your archives will go under the folder `archive` as a .zip file with timestamp filename.
+  8. Change variables in settings.js: `authtoken` & `guildID` and then save.
+  9. Type `node app.js` in your console/terminal, follow instructions and then wait for the program to successfully backup.
+  10. Your archives will go under the folder `archive` as a .zip file with a guild name(guild id) + timestamp filename. If auto is enabled, program will idle until parsed CRON schedule hits.
+ * **Note:** If you're archiving big servers, make sure to change `messagesEveryFile` variable in settings.js.
 
 ### Prerequisites
  * NodeJS version >= 8.1.2
- * Enough disk space.
+ * Enough disk space. **(100000 messages is around 30-40 MB in file size.)**
 
 ### Contributors
 [Tonkku107](https://github.com/tonkku107/) - [Website](https://tonkku.me/)
@@ -33,4 +37,4 @@ module.exports = {
 ### Contributing
 Fork project & Send a pull request. Use eslint, thanks.
 
-### License MIT
+### License MIT Karar Al-Remahy
--- app.js
+++ app.js
@@ -6,42 +6,71 @@
 let readline = require('readline')
 let settings = require('./settings.js')
 
-let rl = readline.createInterface({
+let rlSettings = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-if (settings.guildID === '' && settings.authtoken === '') {
-  console.error(settings.guildID === '' ? '\nNo guildID provided in settings.js' : null)
-  console.error(settings.authtoken === '' ? '\nNo authtoken provided in settings.js\n' : null)
-  process.exit()
+if (settings.authtoken === '') {
+  rlSettings.question('No authtoken provided in settings.js, \nplease enter it manually.\nauthtoken: ', answer => {
+    settings.authtoken = answer
+    if (settings.guildID === '') {
+      let question = () => rlSettings.question('\nNo guildID provided in settings.js, \nplease enter it manually.\nguildID: ', answer => {
+        if (Number.isInteger(Number(answer))) {
+          settings.guildID = answer
+          rlSettings.close()
+          termsOfService()
+        } else question()
+      })
+      question()
+    } else rlSettings.close()
+  })
+} else if (settings.guildID === '') {
+  let question = () => rlSettings.question('No guildID provided in settings.js, \nplease enter it manually.\nguildID: ', answer => {
+    if (Number.isInteger(Number(answer))) {
+      settings.guildID = answer
+      rlSettings.close()
+      termsOfService()
+    } else question()
+  })
+  question()
 }
 
-rl.question("Have you read Discord's Developer Terms of Service?\n" +
-  '\nhttps://discordapp.com/developers/docs/legal\n' +
-  '\nDo you understand that you may have to take all\n' +
-  'responsibility for any and all consequences as a\n' +
-  'result of running this script? (y/n)\n: ', (answer) => {
-  // TODO: Log the answer in a database
+function termsOfService () {
+  if (!settings.acceptTOS) {
+    let rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
 
-  switch (answer.charAt(0)) {
-    case 'y':
-      console.log('Script will timeout for 30 seconds.\n\nIf you change your mind, please press\nCtrl + C on your keyboard to close script.')
-      setTimeout(() => {
-        start()
-        rl.close()
-      }, 30 * 1000)
-      break
-    case 'n':
-      console.log('\nShutting down prematurely.\nPlease uninstall script.')
-      rl.close()
-      break
-    default:
-      console.log('Please run the script again.')
-      rl.close()
-      break
+    rl.question("\nHave you read and accepted Discord's Developer Terms of Service?\n" +
+    '\nhttps://discordapp.com/developers/docs/legal\n' +
+    '\nDo you understand that you may have to take all\n' +
+    'responsibility for any and all consequences as a\n' +
+    'result of running this script? (y/n)\n: ', (answer) => {
+      switch (answer.charAt(0)) {
+        case 'y':
+          console.log('Script will timeout for 30 seconds.\n\nIf you change your mind, please press\nCtrl + C on your keyboard to close script.')
+          setTimeout(() => {
+            start()
+            rl.close()
+          }, 30 * 1000)
+          break
+        case 'n':
+          console.log('\nShutting down prematurely.\nPlease uninstall script.')
+          rl.close()
+          break
+        default:
+          console.log('Please run the script again.')
+          rl.close()
+          break
+      }
+    })
+  } else {
+    console.log('ToS already accepted.')
+    start()
   }
-})
+}
 
 function start () {
   let archiver = require('archiver')
@@ -59,6 +88,9 @@ function start () {
   const auth = settings.authtoken
 
   let object = {}
+  let counter = {}
+  let people = {}
+  let date = Date.now()
 
   let disconnected = false
 
@@ -82,6 +114,7 @@ function start () {
       console.log('Creating temp directory.')
       fs.mkdirSync(path.join(__dirname, 'temp'))
     }
+    // Creates 'archive' directory if it doesn't exist.
     if (!fs.existsSync(path.join(__dirname, 'archive'))) {
       console.log('Creating archive directory.')
       fs.mkdirSync(path.join(__dirname, 'archive'))
@@ -90,22 +123,128 @@ function start () {
       .channels.map(channel => {
         if (channel.permissionsFor(client.user.id).has('READ_MESSAGES') && channel.permissionsFor(client.user.id).has('READ_MESSAGE_HISTORY') && channel.type === 'text') {
           debug(channel.id, channel.name)
-          object[channel.id] = {}
+          object[channel.id] = []
+          people[channel.id] = {}
+          counter[channel.id] = {
+            nextCount: settings.messagesEveryFile,
+            count: 1,
+            atSplit: 0
+          }
           return fetchMore(channel)
         }
-      })).then(() => {
-      debug('Starting compression! Please wait, this may take time.')
-      let output = fs.createWriteStream(path.join(__dirname, 'archive', `archive_${Date.now()}.zip`))
-      output.on('close', () => {
-        console.log('Finished archiving!', Date().toString())
-        if (!settings.auto) process.exit(0)
-        timer()
       })
-      archive.pipe(output)
-      archive.glob('**/*', { cwd: path.join(__dirname, 'temp'), src: ['**/*'], expand: true })
-      archive.finalize((err, bytes) => {
+    ).then((g) => {
+      g = g.filter(function (e) { return e }) // Filter empties.
+      let guild = g.pop()
+      debug('Creating guild details file....')
+      let rolesInGuild = []
+      guild.roles.forEach(item => {
+        rolesInGuild.push({
+          position: item.position,
+          id: item.id,
+          name: item.name,
+          createdTimestamp: item.createdTimestamp,
+          color: item.hexColor,
+          hoistUsers: item.hoist,
+          members: item.members.size,
+          managed: item.managed,
+          mentionable: item.mentionable,
+          permissions: item.permissions
+        })
+      })
+      let emojisInGuild = []
+      guild.emojis.forEach(item => {
+        let availableForRoles = []
+        item.roles.forEach(roles => {
+          availableForRoles.push({
+            id: roles.id,
+            name: roles.name
+          })
+        })
+        emojisInGuild.push({
+          id: item.id,
+          name: item.name,
+          animated: item.animated,
+          requiresColons: item.requiresColons,
+          url: item.url,
+          identifier: item.identifier,
+          createdTimestamp: item.createdTimestamp,
+          managed: item.managed,
+          roles: availableForRoles.length > 0 ? availableForRoles : undefined
+        })
+      })
+      let channelsInGuild = []
+      guild.channels.forEach(item => {
+        let permissionOverwrites = []
+        item.permissionOverwrites.forEach(overwrites => {
+          permissionOverwrites.push({
+            id: overwrites.id,
+            type: overwrites.type
+          })
+        })
+        channelsInGuild.push({
+          position: item.position,
+          id: item.id,
+          type: item.type,
+          name: item.name,
+          createdTimestamp: item.createdTimestamp,
+          parent: item.parent ? { id: item.parent.id, name: item.parent.name } : undefined,
+          permissionOverwrites: permissionOverwrites.length > 0 ? permissionOverwrites : undefined
+        })
+      })
+      let guildDetails = {
+        id: guild.id,
+        name: guild.name,
+        acronym: guild.nameAcronym,
+        icon: guild.iconURL,
+        isLarge: guild.large,
+        memberCount: guild.memberCount,
+        createdTimestamp: guild.createdTimestamp,
+        afk: {
+          channel: guild.afkChannel,
+          timeout: guild.afkTimeout,
+          channelId: guild.afkChannelID
+        },
+        owner: {
+          id: guild.owner.id,
+          name: guild.owner.name,
+          tag: guild.owner.tag,
+          avatar: guild.owner.displayAvatarURL
+        },
+        region: guild.region,
+        splash: guild.splashURL,
+        explicitContentFilter: guild.explicitContentFilter,
+        verificationLevel: guild.verificationLevel,
+        embedEnabled: guild.embedEnabled,
+        emojis: emojisInGuild,
+        roles: rolesInGuild,
+        channels: channelsInGuild,
+        _archivedAt: date,
+        _archivedBy: {
+          id: guild.me.user.id,
+          nickname: guild.me.displayName,
+          name: guild.me.user.username,
+          tag: guild.me.user.tag,
+          avatar: guild.me.user.displayAvatarURL,
+          isBot: guild.me.user.bot
+        },
+        _archiveApp: 'S.A.R.A.H. app by KararTY & Tonkku107 <https://github.com/kararty/serverautorecordarchiverheroine>'
+      }
+      fs.writeFile(path.join(__dirname, 'temp', `[GUILD_INFO]${guild.name}(${guild.id}).json`), JSON.stringify(guildDetails, null, 2), (err) => {
         if (err) throw err
-      // #Not working# if (settings.debug) console.log('Finished compressing! Total bytes', bytes, '\nNot done yet! Please wait...')
+        debug('Starting compression! Please wait, this may take time.')
+        let output = fs.createWriteStream(path.join(__dirname, 'archive', `archive_${guild.name}(${guild.id})_${date}.zip`))
+        output.on('close', () => {
+          console.log('Finished archiving!', Date().toString())
+          if (!settings.auto) process.exit(0)
+          timer()
+        })
+        archive.pipe(output)
+        archive.glob('**/*', { cwd: path.join(__dirname, 'temp'), src: ['**/*'], expand: true })
+        archive.finalize((err, bytes) => {
+          if (err) throw err
+          // #Not working# if (settings.debug) console.log('Finished compressing! Total bytes', bytes, '\nNot done yet! Please wait...')
+        })
       })
     })
   }
@@ -122,7 +261,7 @@ function start () {
     return new Promise((resolve, reject) => {
       channel.fetchMessages({limit: 100, before: before}).then(msg => {
         if (msg.size > 0) {
-          let msgLast = msg.last().id
+          let msgLast = msg.last()
           msg.forEach(msg => {
             if (!msg.system) {
               // Check https://discord.js.org/#/docs/main/stable/class/Message to see what you can archive.
@@ -132,24 +271,43 @@ function start () {
                   attachments.push(element.url)
                 })
               }
+              let firstMention
+              if (!people[channel.id][msg.author.id]) {
+                people[channel.id][msg.author.id] = { id: msg.author.id, avatar: msg.author.displayAvatarURL, isBot: msg.author.bot, createdTimestamp: msg.author.createdTimestamp, name: msg.author.username, tag: msg.author.tag }
+                firstMention = people[channel.id][msg.author.id]
+              }
               let edits = []
               /* Doesn't work.
-            if (msg.editedTimestamp) {
-              msg.edits.forEach((element) => {
-                edits.push({[element.editedTimestamp]: element.cleanContent})
-              })
-            }
-            */
-              object[channel.id][msg.createdTimestamp] = {in: {id: msg.channel.id, name: msg.channel.name}, msgId: msg.id, user: {id: msg.author.id, name: msg.author.username}, content: {message: msg.cleanContent, attachments: attachments.length ? attachments : undefined}, pinned: msg.pinned ? true : undefined, edits: edits.length ? edits : undefined}
+              if (msg.editedTimestamp) {
+                msg.edits.forEach((element) => {
+                  edits.push({[element.editedTimestamp]: element.cleanContent})
+                })
+              }
+              */
+              object[channel.id].push({timestamp: msg.createdTimestamp, in: {id: msg.channel.id, name: msg.channel.name}, msgId: msg.id, user: firstMention || {id: msg.author.id, name: msg.author.username}, content: {message: msg.cleanContent, attachments: attachments.length ? attachments : undefined}, pinned: msg.pinned ? true : undefined, edits: edits.length ? edits : undefined})
+              counter[channel.id].count++
             }
           })
-          debug(msgLast) // Used to let you know that it's still going.
-          fetchMore(channel, msgLast).then(resolve, reject)
+          debug(`${counter[channel.id].count} / ${counter[channel.id].nextCount}\t Split ${counter[channel.id].atSplit}\t for channel ${channel.name}`)
+          if (counter[channel.id].count >= counter[channel.id].nextCount) {
+            counter[channel.id].nextCount += settings.messagesEveryFile
+            counter[channel.id].atSplit++
+            debug('Split', counter[channel.id].atSplit, 'for channel', channel.id, '& next split at', counter[channel.id].nextCount)
+            fs.writeFile(path.join(__dirname, 'temp', `[CHANNEL]${channel.name}${channel.nsfw ? '[NSFW]' : ''}(${channel.id})_${counter[channel.id].atSplit}.json`), JSON.stringify(object[channel.id], null, 2), (err) => {
+              if (err) throw err
+              console.log('Saved split for channel', channel.id, 'at', counter[channel.id].count)
+              object[channel.id] = [] // Reset
+              fetchMore(channel, msgLast.id).then(resolve, reject)
+            })
+          } else fetchMore(channel, msgLast.id).then(resolve, reject)
         } else {
-          fs.writeFile(path.join(__dirname, 'temp', `${channel.id}.json`), JSON.stringify(object[channel.id], null, 2), (err) => {
+          counter[channel.id].atSplit++
+          fs.writeFile(path.join(__dirname, 'temp', `[CHANNEL]${channel.name}${channel.nsfw ? '[NSFW]' : ''}(${channel.id})_${counter[channel.id].atSplit}.json`), JSON.stringify(object[channel.id], null, 2), (err) => {
             if (err) throw err
-            debug('Finished:', channel.id)
-            resolve()
+            console.log('Finished:', channel.id)
+            object[channel.id] = [] // Reset
+            people[channel.id] = {} // Reset
+            resolve(channel.guild)
           })
         }
       })
@@ -160,6 +318,7 @@ function start () {
     console.log('Logged into Discord.')
     if (settings.auto) {
       console.log('Starting automation...')
+      date = Date.now()
       timer()
     } else {
       console.log('Running one-timer...')
